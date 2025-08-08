df5b4d64a7cb15c0a91749a47f4ad7a23da1ac74
KHY90 <power4206@gmail.com>
2025-08-09 02:09:12 +0900

    문장 추천 수정정

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import pipeline
+from app.api.v1.endpoints import pipeline, comprehensive
 
 api_router = APIRouter()
 api_router.include_router(pipeline.router, prefix="/pipeline", tags=["pipeline"])
+api_router.include_router(comprehensive.router, prefix="/comprehensive", tags=["comprehensive"])
